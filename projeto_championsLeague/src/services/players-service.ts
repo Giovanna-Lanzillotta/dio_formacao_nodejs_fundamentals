ecaaa77322dcdef31c8eca1e1aadf5cfe91b50c8
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2025-11-13 23:21:49 -0300

    modificações

--- projeto_championsLeague/src/services/players-service.ts
+++ projeto_championsLeague/src/services/players-service.ts
@@ -1,6 +1,17 @@
+import { noContent, ok } from "../utils/http-helper";
 
 export const getPlayerService = async () => {
-    return {
+    const data = {
         player : "ronaldo"
     };
+    let response = null
+
+    if(data){
+      response  = await ok(data);
+    }else{
+        response = await noContent();
+    }
+
+   
+    return response;
 };
--- projeto_championsLeague/src/utils/http-helper.ts
+++ projeto_championsLeague/src/utils/http-helper.ts
@@ -9,4 +9,11 @@ export const ok = async (data:any): Promise<HttpResponse> => {
         statusCode: 200,
         body: data
     };
-}
+};
+
+export const noContent = async():Promise<HttpResponse> => {
+    return {
+        statusCode: 204,
+        body: null
+    };
+};
